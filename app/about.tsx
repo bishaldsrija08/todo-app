c420ffad8b1bfaaed08da31f4f1f480fff0b1c4e
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-07-27 22:55:49 +0545

    first commit

---
+++ app/about.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const about = () => {
+  return (
+    <View>
+      <Text>This is about section</Text>
+    </View>
+  )
+}
+
+export default about
---
+++ app/index.tsx
@@ -0,0 +1,27 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.content}>Emulator ma run vayo.</Text>
+      <Text>Emulator ma run vayo.</Text>
+      <Text>hi</Text>
+      <Link href={"/about"}>Hello about</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+    fontSize: 25
+  },
+  content: {
+    fontSize: 25
+  }
+});
